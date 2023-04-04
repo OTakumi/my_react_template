7cde2b6d732d7e43d415f8f5c695053ccf8206f9
OTakumi <takumi.o.0410@gmail.com>
2023-04-04 23:06:42 +0900

    update storybook settings

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,30 @@
+const path = require('path')
+
 module.exports = {
-  "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
-  "framework": "@storybook/react",
+    "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+    "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
+    "framework": "@storybook/react",
     "typescript": {
         "reactDocgen": "react-docgen",
     },
-  core: {
-    builder: "webpack5"
-  }
-};
+    core: {
+        builder: "webpack5"
+    },
+    webpackFinal: async (config) => {
+        config.module.rules.push({
+            test: /\.css$/,
+            use: [
+                {
+                    loader: 'postcss-loader',
+                    options: {
+                        postcssOptions: {
+                            plugins: [require('tailwindcss'), require('autoprefixer')],
+                        },
+                    },
+                },
+            ],
+            include: path.resolve(__dirname, '../'),
+        });
+        return config
+    },
+}
